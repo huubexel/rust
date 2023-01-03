f0fe062dec14ef31effe6a7a44898973247e2f5f
Huub Exel <huubexel@hotmail.com>
2023-01-03 20:46:01 +0100

    Added references and borrowing completely

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -11,6 +11,16 @@ fn main() {
 
     change(&mut s);
 
+
+    // Multiple mutable references, for which you need a second scope.
+    let mut s = String::from("hello");
+
+    {
+        let _r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let _r2 = &mut s;
+
 }
 
 fn calculate_length(s: &String) -> usize {
