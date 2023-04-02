a5a13ba5b41d35e23632131ef694aedbf220bc8d
Huub Exel <huubexel@hotmail.com>
2023-04-02 22:48:38 +0200

    Added a bit more about pathing and modules

--- learned_topics/multiple_directory_topics/module_system/src/garden/vegetables.rs
+++ learned_topics/multiple_directory_topics/module_system/src/garden/vegetables.rs
@@ -1,2 +1,18 @@
 #[derive(Debug)]
 pub struct Carrot {}
+
+mod paprika {
+    pub mod seeds {
+        pub fn grow() {}
+    }
+}
+
+pub fn regenerate_food() {
+    /* Absolute path */
+    crate::garden::vegetables::paprika::seeds::grow();
+    /* The absolute might say that the first part is unnecessary,
+       it is way cleaner, you will know exactly where it is. */
+
+    /* Relative path */
+    paprika::seeds::grow();
+}
