d9b07053252024f4015268760fc8ec1ad2fce325
Huub Exel <huubexel@hotmail.com>
2023-04-22 13:29:18 +0200

    Put a part of the code in lib.rs

---
+++ learned_topics/multiple_directory_topics/minigrep/src/lib.rs
@@ -0,0 +1,37 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    /* Opens a file and returns a Result<String> of the file's contents. */
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+/* Normally you would not want to use clone because the runtime performance will be worse,
+but for now this will do. In your absolute final code you would want to use something
+different in this situation. */
+
+/* We first had new as the name of the function to make an instantiation of the Config struct.
+However this function could fail, and programmers expect new to never fail, so that's why we
+changed the name to build.
+ */
--- learned_topics/multiple_directory_topics/minigrep/src/main.rs
+++ learned_topics/multiple_directory_topics/minigrep/src/main.rs
@@ -1,11 +1,12 @@
-use std::{env, process}; // env has args() which can read command line arguments.
-use std::fs; // fs has functions to handle files
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     /* Gets the Arguments given in by the user and put them in a Vector */
     let args: Vec<String> = env::args().collect();
 
-    let config= Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -13,31 +14,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-}
-
-fn run(config: Config) {
-    /* Opens a file and returns a Result<String> of the file's contents. */
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
 
@@ -50,12 +29,3 @@ so in this case we want a vector of strings. */
 
 
 // dbg!(args); /* prints args using the debug macro */
-
-/* Normally you would not want to use clone because the runtime performance will be worse,
-but for now this will do. In your absolute final code you would want to use something
-different in this situation. */
-
-/* We first had new as the name of the function to make an instantiation of the Config struct.
-However this function could fail, and programmers expect new to never fail, so that's why we
-changed the name to build.
- */
